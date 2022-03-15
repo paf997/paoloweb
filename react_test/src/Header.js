c6895f4ec30494fe9b8e8145ea3962dc96b06efa
paolo <paf997@usask.ca>
2022-03-15 09:53:43 -0600

    Started some animations

--- react_test/src/Header.js
+++ react_test/src/Header.js
@@ -7,7 +7,7 @@ function Header(props) {
       <header className={"header"}>
         <a href="http://localhost:3000/#card_div">my_work()</a>
         <a href="https://docs.google.com/document/d/1PPsxCbuu3FZbL6a1WiCNogNH-9vhP5HBu5BIyeYd0ao/edit?usp=sharing">my_resume()</a>
-        <a href="#">contact_me()</a>
+        <a href="http://localhost:3000/#contact">contact_me()</a>
       </header>
     )
 }
--- react_test/src/Home.js
+++ react_test/src/Home.js
@@ -4,25 +4,17 @@ import Header from './Header';
 import Footer from './Footer';
 import Card from './Card';
 import axios from "axios";
-
-type form_state = {
-    name:String;
-    email:String;
-    message:String;
-}
+import useForm  from './useForm';
 
 function Home (props) {
 
+    const {handleChange, values} = useForm();
     const [color_mode, set_color_mode] = useState(true);
-    const [form_state, use_form_state] = useState();
-    const submit = async(event:FormEvent) =>{
-        event.preventDefault();
-        await postSubmission();
-    };
-
     const postSubmission = async() =>{
         const payload = {
-            message:"form spark submission",
+            name:values.name,
+            email:values.email,
+            msg:values.msg
         }
 
         try{
@@ -37,6 +29,12 @@ function Home (props) {
     const formID = "lx11Nz0L";
     const formSparkURL = `https://submit-form.com/${formID}`;
 
+    const submit = async(event) =>{
+        event.preventDefault();
+        console.log("hello " + values.name + " " + values.email);
+        /*await postSubmission();*/
+    };
+
     const switch_color_modes = () => {
       set_color_mode((prev => !prev));
       console.log("Mode" + color_mode);
@@ -46,7 +44,7 @@ function Home (props) {
         <Header></Header>
         <div className="welcome">
             <p className="ciao">Ciao</p>
-            <p className="name">My Name Is Paolo A Fenu </p>
+            <p className="myname">My Name Is Paolo A Fenu </p>
             <p className="thanks">Thanks For Visiting</p>
         </div>
         <div id="card_div">
@@ -63,10 +61,35 @@ function Home (props) {
                 link={"https://ritornello.ca/"}>
              </Card>
         </div>
-        <div className="welcome">Contact Me
-            <form onSubmit={submit}>
-                <button>Submit</button>
-            </form>
+        <div id="contact">
+            <div className="welcome">Contact Me
+                <form className="form" onSubmit={submit}>
+                    <input className="name"
+                        type='text'
+                        placeholder="name"
+                        name="name"
+                        value={values.name}
+                        onChange={handleChange}
+                    ></input>
+
+                    <input className="email"
+                        type='email'
+                        placeholder="email"
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                    ></input>
+
+                    <textarea className="msg"
+                        placeholder="message"
+                        name="msg"
+                        value={values.msg}
+                        onChange={handleChange}
+                    ></textarea>
+
+                    <button className="submit">submit</button>
+                </form>
+            </div>
         </div>
         <Footer></Footer>
       </div>
